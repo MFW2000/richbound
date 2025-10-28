29160a4c64f17bbbe449362603ed8638476d4e75
MFW2000 <88318338+MFW2000@users.noreply.github.com>
2025-10-28 23:16:27 +0100

    Add solution (#2)

---
+++ MFW.Richbound/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MFW.Richbound;
+
+/// <summary>
+/// Represents the primary entry point for running the application.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// The entry point of the application.
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
