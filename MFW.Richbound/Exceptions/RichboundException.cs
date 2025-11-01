e1236653fd1200a541f612f04e8b682978e1d2ff
MFW2000 <88318338+MFW2000@users.noreply.github.com>
2025-11-01 17:10:49 +0100

    Add custom exceptions (#5)

---
+++ MFW.Richbound/Exceptions/RichboundException.cs
@@ -0,0 +1,25 @@
+﻿namespace MFW.Richbound.Exceptions;
+
+/// <summary>
+/// The exception that is thrown when a Richbound specific exception occurs.
+/// </summary>
+public class RichboundException : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RichboundException"/> class.
+    /// </summary>
+    public RichboundException() { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RichboundException"/> class with a specified error message.
+    /// </summary>
+    /// <inheritdoc/>
+    public RichboundException(string message) : base(message) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RichboundException"/> class with a specified error message and a
+    /// reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <inheritdoc/>
+    public RichboundException(string message, Exception innerException) : base(message, innerException) { }
+}
