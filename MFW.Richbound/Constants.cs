5dd3c9e883c2faae14980a110856db4d98c3d65c
MFW2000 <marijnwelmerink@gmail.com>
2025-11-01 19:41:34 +0100

    Add constants and display text providers

---
+++ MFW.Richbound/Constants.cs
@@ -0,0 +1,9 @@
+namespace MFW.Richbound;
+
+/// <summary>
+/// Provides common values used throughout the application.
+/// </summary>
+public class Constants
+{
+    public const string DefaultLogFile = "richbound.log";
+}
