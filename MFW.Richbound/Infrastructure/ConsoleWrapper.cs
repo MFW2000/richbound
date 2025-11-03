a58ca4b6567fa88f91e2c7b2b9471231e3ec1de8
MFW2000 <marijnwelmerink@gmail.com>
2025-11-03 19:11:24 +0100

    Remove unnecessary console wrapper tests

--- MFW.Richbound/Infrastructure/ConsoleWrapper.cs
+++ MFW.Richbound/Infrastructure/ConsoleWrapper.cs
@@ -1,10 +1,12 @@
-﻿using MFW.Richbound.Infrastructure.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using MFW.Richbound.Infrastructure.Interfaces;
 
 namespace MFW.Richbound.Infrastructure;
 
 /// <summary>
 /// Implements <see cref="IConsoleWrapper"/> for wrapping console functionality that can be mocked for testing.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public class ConsoleWrapper : IConsoleWrapper
 {
     /// <inheritdoc/>
