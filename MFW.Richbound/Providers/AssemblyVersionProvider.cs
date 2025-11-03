4b756062b63bb20b515717907f4fe4408d53faef
MFW2000 <marijnwelmerink@gmail.com>
2025-11-03 19:33:01 +0100

    Add assembly version provider

---
+++ MFW.Richbound/Providers/AssemblyVersionProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using MFW.Richbound.Providers.Interfaces;
+
+namespace MFW.Richbound.Providers;
+
+/// <summary>
+/// Implements <see cref="IAssemblyVersionProvider"/> for retrieving the application's assembly version.
+/// </summary>
+/// <param name="assembly">The assembly whose version is retrieved.</param>
+public class AssemblyVersionProvider(Assembly assembly) : IAssemblyVersionProvider
+{
+    /// <inheritdoc/>
+    public Version? GetVersion()
+    {
+        return assembly.GetName().Version;
+    }
+}
