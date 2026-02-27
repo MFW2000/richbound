0ab8ccbf1b3be032159105147ae3abfe22c7fc0d
MFW2000 <marijnwelmerink@gmail.com>
2026-02-27 21:28:47 +0100

    Add documentation

--- MFW.Richbound/Presentation/Main/NewGame.cs
+++ MFW.Richbound/Presentation/Main/NewGame.cs
@@ -8,6 +8,9 @@ using MFW.Richbound.Models;
 
 namespace MFW.Richbound.Presentation.Main;
 
+/// <summary>
+/// Responsible for assisting the user in creating a new character and starting a new game.
+/// </summary>
 public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : Prompt
 {
     /// <inheritdoc/>
@@ -49,6 +52,10 @@ public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : P
         return PromptType.NewGameIntro;
     }
 
+    /// <summary>
+    /// Check if the user wants to overwrite the save file if it exists.
+    /// </summary>
+    /// <returns>True if the user wants to overwrite the save file, otherwise false.</returns>
     private bool PromptOverwriteSave()
     {
         if (!saveFileManager.HasSaveFile())
@@ -63,6 +70,10 @@ public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : P
         return overwriteSave;
     }
 
+    /// <summary>
+    /// Prompt the user if they want to create a male or female character.
+    /// </summary>
+    /// <returns>A <see cref="Gender"/> value representing the user's gender.</returns>
     private static Gender PromptGender()
     {
         Console.WriteLine("Choose your gender (male/female) [m/f]:");
@@ -96,6 +107,10 @@ public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : P
         }
     }
 
+    /// <summary>
+    /// Prompt the user for their character's last name.
+    /// </summary>
+    /// <returns>The user's character's last name.</returns>
     private static string PromptLastName()
     {
         Console.WriteLine($"What is your last name (max {Constants.MaxNameLength} characters):");
@@ -126,6 +141,10 @@ public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : P
         }
     }
 
+    /// <summary>
+    /// Prompt the user for their character's first name.
+    /// </summary>
+    /// <returns>The user's character's first name.</returns>
     private static string PromptFirstName()
     {
         Console.WriteLine($"What is your first name (max {Constants.MaxNameLength} characters):");
@@ -156,6 +175,13 @@ public class NewGame(ISaveFileManager saveFileManager, IGameState gameState) : P
         }
     }
 
+    /// <summary>
+    /// Prompt the user to confirm their character's details.
+    /// </summary>
+    /// <param name="firstName">The chosen first name.</param>
+    /// <param name="lastName">The chosen last name.</param>
+    /// <param name="gender">The chosen gender.</param>
+    /// <returns>True if the user confirms their character's details, otherwise false.</returns>
     private static bool PromptCharacterConfirmation(string firstName, string lastName, Gender gender)
     {
         var promptText = gender == Gender.Male
