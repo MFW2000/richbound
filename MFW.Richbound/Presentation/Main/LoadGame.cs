f19abdc7ae997566487d67b1e9f24665907b232c
MFW2000 <marijnwelmerink@gmail.com>
2026-03-02 00:09:30 +0100

    Implement load game prompt

--- MFW.Richbound/Presentation/Main/LoadGame.cs
+++ MFW.Richbound/Presentation/Main/LoadGame.cs
@@ -1,18 +1,50 @@
+using MFW.Richbound.Domain.Interfaces;
 using MFW.Richbound.Enumerations;
 using MFW.Richbound.Infrastructure.Interfaces;
 
 namespace MFW.Richbound.Presentation.Main;
 
-public class LoadGame(ISaveFileManager saveFileManager) : Prompt
+/// <summary>
+/// Responsible for assisting the user in loading a saved game.
+/// </summary>
+public class LoadGame(ISaveFileManager saveFileManager, IGameState gameState) : Prompt
 {
     /// <inheritdoc/>
     public override PromptType? DisplayMainPrompt()
     {
-        Console.WriteLine("Load game selected.");
+        Console.WriteLine("=== Load Game ===");
+
+        if (!saveFileManager.HasSaveFile())
+        {
+            Console.WriteLine("No save game found. Returning to main menu.");
+            Console.WriteLine();
+
+            ContinuePrompt();
+
+            return PromptType.MainMenu;
+        }
+
+        Console.WriteLine("Loading save game...");
+
+        var saveGame = saveFileManager.LoadGame();
+
+        if (saveGame == null)
+        {
+            Console.WriteLine("Something went wrong while loading the save game. Returning to main menu.");
+            Console.WriteLine();
+
+            ContinuePrompt();
+
+            return PromptType.MainMenu;
+        }
+
+        gameState.Initialize(saveGame);
+
+        Console.WriteLine("Save game loaded successfully.");
         Console.WriteLine();
 
         ContinuePrompt();
 
-        return PromptType.MainMenu;
+        return PromptType.GameMenu;
     }
 }
