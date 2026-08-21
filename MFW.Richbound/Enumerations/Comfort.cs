dd37b4bd7bb258ec964cd0f71fc87a3b563567f5
MFW2000 <marijnwelmerink@gmail.com>
2026-08-21 23:00:44 +0200

    Add comfort enumeration

---
+++ MFW.Richbound/Enumerations/Comfort.cs
@@ -0,0 +1,11 @@
+namespace MFW.Richbound.Enumerations;
+
+/// <summary>
+/// Represents the comfort level of a player's home.
+/// </summary>
+public enum Comfort
+{
+    Poor,
+    Average,
+    Good
+}
